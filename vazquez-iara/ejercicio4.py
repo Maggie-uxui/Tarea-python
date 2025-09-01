c0284214e185dd63cc415b2bdafd86c31786365d
Iara Magali Vazquez <iaravazquezz31@gmail.com>
2025-09-01 20:57:43 -0300

    ejercicio4.py

---
+++ vazquez-iara/ejercicio4.py
@@ -0,0 +1,39 @@
+# 1)
+# nombre=input("Ingrese su nombre")
+
+# numero=int(float(input("Ingrese la cantidad a repetir")))
+
+# for numero in range(numero):
+#     print(nombre)
+#2)
+# for l in range(0,26):
+#     print(l)    
+#3)
+# numero=int(input("Ingrese un Numero"))
+
+# for N in range(0,numero+1):
+#     print(N)
+
+#4)
+
+# for N in range(0,11):
+#      print("2x", N, "=", N*2)
+
+#5)
+
+# numero=int(input("Ingrese un Numero"))
+# for N in range(0,11):
+#      print(numero, "x", N, "=", N*numero)
+
+#Extra
+tablaDesde=1
+tablaHasta=10
+desde=1
+hasta=10
+for tabla in range(tablaDesde, tablaHasta+1):
+    print("Tabla del", tabla)
+    for tablaMulti in range(desde, hasta+1):
+        print(tabla, "x", tablaMulti , "=", tabla*tablaMulti) 
+
+# for tabla in range(0,10):
+#      tabla(10)
