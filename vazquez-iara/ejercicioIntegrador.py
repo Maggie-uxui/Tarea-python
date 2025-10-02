d741ffd38b44cfdeeb1abb6040ed7e5e9d2374e9
Iara Magali Vazquez <iaravazquezz31@gmail.com>
2025-10-02 00:22:50 -0300

    Update ejercicioIntegrador.py

--- vazquez-iara/ejercicioIntegrador.py
+++ vazquez-iara/ejercicioIntegrador.py
@@ -140,7 +140,7 @@ def dividir(a, b):
 opcion = 0
 
 while opcion != 5:
-    print("\n--- Calculadora ---")
+    print("--- Calculadora ---")
     print("1. Sumar")
     print("2. Restar")
     print("3. Multiplicar")
