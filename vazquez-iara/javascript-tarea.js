e2ee56ec477222b01ad77025881cabded65f5353
Iara Magali Vazquez <iaravazquezz31@gmail.com>
2025-11-13 16:30:53 -0300

    Update javascript-tarea.js

--- vazquez-iara/javascript-tarea.js
+++ vazquez-iara/javascript-tarea.js
@@ -67,3 +67,63 @@ function esPar(num){
 esPar(0)
 
 //-----------------------array---------------------------
+//1 y 2
+
+// let frutas=["manzanas","peras","bananas","duraznito"];
+// console.log(frutas[0],frutas[frutas.length-1]);
+
+// frutas.push("naranjas");
+// console.log(frutas.length);
+
+//3 y 4
+
+// let colores=[], coloresUser, colorUser, estaColor;
+
+// for (let i=0; i<5; i++){
+//     coloresUser=prompt("Ingrese un color");
+//     colores.push(coloresUser);
+// }
+
+// for (let i=0; i<5; i++){
+//     console.log(colores[i])
+// }
+
+// colorUser=prompt("Ingrese un color que quiera Buscar");
+// for (let i=0; i<coloresUser.length; i++){
+//     if (coloresUser[i]==colorUser){
+//         estaColor=true
+//     }
+// }
+// if (estaColor==true) {
+//     alert("El color "+ colorUser + " esta en la lista.")
+// }else{
+//     alert("El color "+ colorUser + " no esta en la lista.")
+// }
+
+// 5
+
+let numeros=[4,98,257,-85,0,21,-582,148,-293], numeroMayor, suma, positivos=[], pares=[];
+positivos=0
+suma=0
+numeroMayor=numeros[0]
+
+
+for (i=0; i<numeros.length; i++){
+    
+    if (numeros[i]>numeroMayor) {
+        numeroMayor=numeros[i];
+    }
+
+    suma=suma+numeros[i];
+
+    if (numeros[i]>0) {
+        positivos=positivos+1;
+    }
+
+    if (numeros[i] % 2 == 0) {
+        pares.push(numeros[i]);
+    }
+}
+
+console.log(pares)
+
